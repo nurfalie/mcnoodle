a39ce83d9770e21d3f4191c22cf1f826251fcb76
Guess Who <textbrowser@gmail.com>
2016-07-07 20:27:50 -0400

    Preparing S... must be non-singular.

--- mcnoodle.cc
+++ mcnoodle.cc
@@ -1,3 +1,11 @@
+extern "C"
+{
+#include <inttypes.h>
+}
+
+#include <boost/nondet_random.hpp>
+#include <boost/random.hpp>
+
 #include "mcnoodle.h"
 
 mcnoodle::mcnoodle(const size_t k, const size_t n, const size_t t)
@@ -14,5 +22,12 @@ mcnoodle::~mcnoodle()
 
 void mcnoodle::prepareS(void)
 {
+  boost::random::uniform_int_distribution<uint64_t> distribution;
+  boost::random_device random_device;
+
   S.resize(m_k, m_k);
+
+  for(size_t i = 0; i < S.size1(); i++)
+    for(size_t j = 0; j < S.size2(); j++)
+      S(i, j) = distribution(random_device) % 2;
 }
