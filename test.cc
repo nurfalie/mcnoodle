ca25ba9e9eee7b00e0b16ed3970d74ac60ff1397
Guess Who <textbrowser@gmail.com>
2016-08-24 13:47:00 -0400

    Updated.

--- test.cc
+++ test.cc
@@ -20,12 +20,33 @@ int test1(void)
 
   rc &= m.encrypt(plaintext, strlen(plaintext), c);
   rc &= m.decrypt(c, p);
+
+  if(rc &= (p.str() == std::string(plaintext)))
+    std::cout << "p equals plaintext!" << std::endl;
+  else
+    std::cout << "p does not equal plaintext!" << std::endl;
+
   return rc;
 }
 
 int test2(void)
 {
   int rc = 1;
+  mcnoodle m(12, 200);
+
+  rc = m.generatePrivatePublicKeys();
+
+  char plaintext[] = "A longer sentence. Some things are fine.";
+  std::stringstream c;
+  std::stringstream p;
+
+  rc &= m.encrypt(plaintext, strlen(plaintext), c);
+  rc &= m.decrypt(c, p);
+
+  if(rc &= (p.str() == std::string(plaintext)))
+    std::cout << "p equals plaintext!" << std::endl;
+  else
+    std::cout << "p does not equal plaintext!" << std::endl;
 
   return rc;
 }
